14677eff191dae0eebbb6e64fd8a0a197c685a79
Tanner Hoelzel <tannerhoelzel@gmail.com>
2025-10-12 16:51:58 -0400

    move gpx map rendering to component & gpx icons

--- components/MapView.tsx
+++ components/MapView.tsx
@@ -1,39 +1,50 @@
 // src/ChartView.js
 'use client';
 
-import React from "react";
+import { React, useEffect } from "react";
 import "leaflet/dist/leaflet.css"
-import { MapContainer, Marker, TileLayer, Tooltip, Popup } from "react-leaflet"
+import { MapContainer, Marker, TileLayer, Tooltip, Popup, useMap } from "react-leaflet"
 import * as L from "leaflet";
 import "leaflet-gpx";
 
 //import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
-
-/*
 function GpxTrack({ url }: { url: string }) {
-  // Custom hook to add GPX to the map
-  const MapComponent = (await import("react-leaflet")).useMap();
+  const customIcon = new L.Icon({
+        iconUrl:
+          "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+        iconRetinaUrl:
+          "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+        shadowUrl:
+          "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41],
+  });
+  const map = useMap();
+
   useEffect(() => {
     const gpx = new L.GPX(url, {
       async: true,
-      marker_options: {
-        startIconUrl:
-          "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-        endIconUrl:
-          "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-        shadowUrl:
-          "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+      markers: {
+        startIcon: customIcon,
+        endIcon: customIcon,
+        // shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
       },
     })
-      .on("loaded", (e: any) => {
-        MapComponent.fitBounds(e.target.getBounds());
+      .on("loaded", (e: { target: L.GPX }) => {
+        map.fitBounds(e.target.getBounds());
       })
-      .addTo(MapComponent);
-  }, [url, MapComponent]);
+      .addTo(map);
+
+    return () => {
+      map.removeLayer(gpx);
+    };
+  }, [url, map]);
 
   return null;
 }
- */
+
 export default function MapView() {
   const customIcon = new L.Icon({
         iconUrl:
@@ -49,12 +60,12 @@ export default function MapView() {
   });
   const gpxUrl = "/day1.gpx";
   return (
-    <MapContainer style={{ height: '100vh' }} center={[41.2195553, -73.9674118]} zoom={15} scrollWheelZoom={false}>
+    <MapContainer style={{ height: '100vh' }} center={[41.2195553, -73.9674118]} zoom={15} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      
+      <GpxTrack url={gpxUrl} />    
       <Marker position={[41.2195553, -73.9674118]} icon={customIcon}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
