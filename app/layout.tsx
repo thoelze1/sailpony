737c272dd46e802d8461e1e105b8fcf9974d5f17
Tanner Hoelzel <tannerhoelzel@gmail.com>
2025-10-25 16:45:32 -0400

    correctly force dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gray-50 text-gray-900`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gray-50 text-gray-900 dark`}
       >
         <NavBar />
         <main className="p-6">{children}</main>
